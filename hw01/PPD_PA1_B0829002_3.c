53492be07da72e375897ae2aa35c6ddc8372f2aa
JaeggerJose <lms025187@gmail.com>
2024-04-23 16:11:25 +0800

    :memo: modify the standard output and add documentation

--- hw01/PPD_PA1_B0829002_3.c
+++ hw01/PPD_PA1_B0829002_3.c
@@ -20,12 +20,12 @@ int main(int argc, char** argv) {
     }
 
 	MPI_Scatter(sendBuffer, maxMessageLength, MPI_CHAR, recvBuffer, maxMessageLength, MPI_CHAR, 0, MPI_COMM_WORLD);
-	snprintf(slaveBuffer, maxMessageLength, "Rank %d received. Thank you!", proId);
-
-	MPI_Gather(slaveBuffer, maxMessageLength, MPI_CHAR, gatherBuffer, maxMessageLength, MPI_CHAR, 0, MPI_COMM_WORLD);
+	if (proId != 0)
 		printf("[MPI_Wtime():%f] Rank %d got message from rank 0: '%s'\n", MPI_Wtime() - startTime ,proId, recvBuffer);
+	snprintf(slaveBuffer, maxMessageLength, "Rank %d received. Thank you!", proId);
+	MPI_Gather(slaveBuffer, maxMessageLength, MPI_CHAR, gatherBuffer, maxMessageLength, MPI_CHAR, 0, MPI_COMM_WORLD);
 	if (proId == 0) {
-		for(int i=0;i<numPro;i++)
+		for(int i=1;i<numPro;i++)
 			printf("[MPI_Wtime():%f] Rank 0 got message from rank %d: '%s'\n", MPI_Wtime() - startTime, i, gatherBuffer[i]);
 	}
 	MPI_Finalize();
